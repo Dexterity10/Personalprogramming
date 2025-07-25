d10af492cd02a23ebf8cd6f819a5b940ce773744
Dexterity <alex.goemans@gmail.com>
2025-07-25 12:06:19 -1000

    boop

--- Java/dailygames/src/main/java/Whodunnit/Main.java
+++ Java/dailygames/src/main/java/Whodunnit/Main.java
@@ -1,6 +1,21 @@
 package Whodunnit;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Suspect;
+
 public class Main {
+
+    private Suspect[] getList() {
+        List<Suspect>[] susList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Suspect person = new Suspect("Person " + i);
+            susList.add(person);
+        }
+        return susList;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
